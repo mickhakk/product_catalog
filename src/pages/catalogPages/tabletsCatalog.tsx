9ca38b2ab908d98387f834e11ad661c369b8651b
Viktor Shaparenko <vkshadyafk@gmail.com>
2024-01-23 17:31:38 +0200

    add some styles

--- src/pages/catalogPages/tabletsCatalog.tsx
+++ src/pages/catalogPages/tabletsCatalog.tsx
@@ -1,3 +1,12 @@
-export const TabletsCatalog = () => (
-  <h1>Tablets Catalog Page</h1>
-);
+import { Pagination } from "../../components/Pagination/Pagination";
+import { useContextProvider } from "../../context/ProductsContext";
+
+export const TabletsCatalog = () => {
+  const { products } = useContextProvider()
+  console.log(products.length)
+  return (
+  <>
+    <h1>Tablets Catalog Page</h1>
+    <Pagination totalCount={98} pageSize={16} siblingCount={1} currentPage={1}/>
+  </>
+)};
