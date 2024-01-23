382fdd3d74c1ead9c658f4e2886357a81b69926c
Viktor Shaparenko <vkshadyafk@gmail.com>
2024-01-23 22:46:04 +0200

    change some logic and add styles onchange

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useLocation, useParams } from 'react-router-dom';
 import { usePagination } from '../../CustomHooks/UsePagination';
+import cn from 'classnames';
 import styles from '../Pagination/pagination.module.scss';
+
 interface Props {
   totalCount: number,
   pageSize: number,
@@ -10,8 +12,11 @@ interface Props {
 }
 export const Pagination: FC<Props> = (props) => {
   const { productId } = useParams();
+  const location = useLocation();
+  const { pathname } = location;
+  const partOfUrl = pathname.split('/')
   const id = Number(productId?.slice(-1))
-  console.log(id);
+
   const {
     totalCount,
     pageSize,
@@ -27,9 +32,10 @@ export const Pagination: FC<Props> = (props) => {
   });
 
   const pages: number[] = paginationRange.map(page => Number(page));
-
+  const lastPage = Math.max(...pages);
+  const firstPage = Math.min(...pages);
  const onNext = () => {
-  const lastPage = Math.max(...pages)
+ 
   if (id === lastPage) {
     return;
   }
@@ -39,11 +45,23 @@ export const Pagination: FC<Props> = (props) => {
   return;
  }
 
+ const onPrevious =()=> {
+   if(id === firstPage) {
+    return;
+   }
+   if(id) {
+    return id - 1;
+   }
+
+   return;
+ }
+
   return (
 
     <ul className={styles['pagination-container']}>
       <li>
-      <Link className={styles['pagination-item']} to='/'>
+      <Link className={styles['pagination-item']}
+       to={`/${partOfUrl[1]}/page=${onPrevious()}`}>
       <svg className={styles.arrow} xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
         <path fill-rule="evenodd" clip-rule="evenodd" d="M10.4715 3.52861C10.2111 3.26826 9.78903 3.26826 9.52868 3.52861L5.52868 7.52861C5.26833 7.78896 5.26833 8.21107 5.52868 8.47141L9.52868 12.4714C9.78903 12.7318 10.2111 12.7318 10.4715 12.4714C10.7318 12.2111 10.7318 11.789 10.4715 11.5286L6.94289 8.00001L10.4715 4.47141C10.7318 4.21107 10.7318 3.78896 10.4715 3.52861Z" fill=""/>
       </svg>
@@ -51,13 +69,15 @@ export const Pagination: FC<Props> = (props) => {
       </li>
       {paginationRange.map(currenNumber => (
         <li key={currenNumber}>
-          <Link className={styles['pagination-item']} to={`/tablets/page=${currenNumber}`}>
+          <Link className={cn(styles['pagination-item'], {
+        [`${styles['pagination-item--is-active']}`]: id === currenNumber,
+      })} to={`/${partOfUrl[1]}/page=${currenNumber}`}>
             {currenNumber}
           </Link>
         </li>
       ))}
       <li>
-        <Link className={styles['pagination-item']} to={`/tablets/page=${onNext()}`}>
+        <Link className={styles['pagination-item']} to={`/${partOfUrl[1]}/page=${onNext()}`}>
         <svg className={styles.arrow} xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
           <path  fill-rule="evenodd" clip-rule="evenodd" d="M5.52876 3.52861C5.78911 3.26826 6.21122 3.26826 6.47157 3.52861L10.4716 7.52861C10.7319 7.78896 10.7319 8.21107 10.4716 8.47141L6.47157 12.4714C6.21122 12.7318 5.78911 12.7318 5.52876 12.4714C5.26841 12.2111 5.26841 11.789 5.52876 11.5286L9.05735 8.00001L5.52876 4.47141C5.26841 4.21107 5.26841 3.78896 5.52876 3.52861Z" fill=''/>
         </svg>
