9fda296bdfef98a26c21d747195549b078f5e075
Viktor Shaparenko <vkshadyafk@gmail.com>
2024-01-23 12:51:48 +0200

    create hook usePagination

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+
+interface Props {
+  totalCount: number,
+  pageSize: number,
+  siblingCount: number,
+  currentPage: number,
+}
+export const Pagination: FC<Props> = ({
+  totalCount,
+  pageSize,
+  siblingCount,
+  currentPage,
+}) => {
+  
+return (
+
+)
+};
