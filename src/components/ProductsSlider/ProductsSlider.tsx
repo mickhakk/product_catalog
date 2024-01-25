82596d6e6593a12a66103887d484fa46811fdb95
Kseniia Nikolaienko <ksysha.nikolaenko778@gmail.com>
2024-01-25 21:28:56 +0200

    add basic slider functionality

--- src/components/ProductsSlider/ProductsSlider.tsx
+++ src/components/ProductsSlider/ProductsSlider.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import './swiper.scss';
 import styles from './ProductsSlider.module.scss';
+import { ProductCard } from '../ProductCard/ProductCard';
+import { Product } from '../../types/Product';
+import { SliderButtons } from './SliderButtons/SliderButtons';
 
 interface Props {
   children: string;
 }
+
+const product: Product = {
+  id: 1,
+  category: 'phones',
+  itemId: 'apple-iphone-11-pro-max-256gb-silver',
+  name: 'Apple iPhone 11 Pro Max 256GB Silver',
+  fullPrice: 999,
+  price: 700,
+  screen: '6.1” OLED',
+  capacity: '256GB',
+  color: 'silver',
+  ram: '6GB',
+  year: 2020,
+  image: 'img/phones/apple-iphone-7/black/00.jpg',
+};
 
 export const ProductsSlider: React.FC<Props> = ({ children }) => {
   return (
@@ -12,15 +34,47 @@ export const ProductsSlider: React.FC<Props> = ({ children }) => {
         <h2 className={styles.products_slider__title}>
           {children}
         </h2>
-
-        <div className={styles.products_slider__buttons}>
-          buttons
-        </div>
       </div>
 
-      <div className={styles.products_slider__products}>
-        Products
-      </div>
+      <Swiper
+        spaceBetween={16}
+        slidesPerView="auto"
+        breakpoints={{
+          320: { slidesPerGroup: 1 },
+          640: { slidesPerGroup: 2 },
+          1200: { slidesPerGroup: 4 },
+        }}
+      >
+        <SliderButtons />
+
+        <SwiperSlide>
+          <ProductCard product={product} simpleHoverEffect />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductCard product={product} simpleHoverEffect />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductCard product={product} simpleHoverEffect />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductCard product={product} simpleHoverEffect />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductCard product={product} simpleHoverEffect />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductCard product={product} simpleHoverEffect />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductCard product={product} simpleHoverEffect />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductCard product={product} simpleHoverEffect />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductCard product={product} simpleHoverEffect />
+        </SwiperSlide>
+      </Swiper>
     </section>
   );
 };
---
+++ src/components/ProductsSlider/SliderButtons/SliderButtons.tsx
@@ -0,0 +1,25 @@
+import { useSwiper } from 'swiper/react';
+import styles from './SliderButtons.module.scss';
+
+export const SliderButtons = () => {
+  const swiper = useSwiper();
+
+  return (
+    <div className={styles.slider_buttons}>
+      <button
+        type="button"
+        onClick={() => swiper.slidePrev()}
+        className={styles.slider_button}
+      >
+        {'<'}
+      </button>
+      <button
+        type="button"
+        onClick={() => swiper.slideNext()}
+        className={styles.slider_button}
+      >
+        {'>'}
+      </button>
+    </div>
+  );
+};
